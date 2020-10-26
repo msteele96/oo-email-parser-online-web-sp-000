2dd400539bc151c1864980f13c826c2f4feea778
Marc Steele <marcsteele2014@gmail.com>
2020-10-26 15:34:29 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -17,7 +17,7 @@ class EmailAddressParser
     emails_array = []
     emails_array << @emails.split(" ")
     emails_array.each do |email|
-      email.gsub("," , "")
+      email.delete(",")
       binding.pry
     end
   end
